b70263e1074d5dc345a52cbbf8e2714dbb1f1679
AL3X1 <alex@mail.co>
2023-04-03 11:38:02 +0300

    feat: added readme and moved token fetching from EV

--- src/processor.rs
+++ src/processor.rs
@@ -1,9 +1,8 @@
-use std::io;
+use std::{env, io};
 use std::io::Write;
 use termion::color;
 use crate::models::gpt::{GPTError, GPTMessage, GPTRequest, GPTResponse};
 
-const TOKEN: &str = "Bearer "; // the GPT token should be here
 const REQUEST_URL: &str = "https://api.openai.com/v1/chat/completions";
 const GPT_MODEL: &str = "gpt-3.5-turbo";
 const CONTENT_TYPE: &str = "application/json";
@@ -12,12 +11,23 @@ const STOP_CMD: &str = "exit";
 
 pub struct QueryProcessor {
     debug_mode: bool,
+    gpt_token: String
 }
 
 impl QueryProcessor {
     pub fn new(debug_mode: bool) -> Self {
+        let token_ev = env::var("GPT_KEY");
+
+        if token_ev.is_err() {
+            panic!("Unable to find gpt api key. Please add it manually with `export GPT_KEY=yourkey`")
+        }
+        
+        let mut gpt_token = "Bearer ".to_owned();
+        gpt_token.push_str(&token_ev.unwrap());
+        
         QueryProcessor {
             debug_mode,
+            gpt_token
         }
     }
 
@@ -49,7 +59,7 @@ impl QueryProcessor {
                 ]),
             };
 
-            let response_result = QueryProcessor::process(&model).await;
+            let response_result = self.process(&model).await;
             
             if response_result.is_err() {
                 continue;
@@ -65,14 +75,14 @@ impl QueryProcessor {
         }
     }
 
-    async fn process(query: &GPTRequest) -> serde_json::Result<GPTResponse> {
+    async fn process(&self, query: &GPTRequest) -> serde_json::Result<GPTResponse> {
         println!("{}Loading...", color::Fg(color::LightGreen));
         let json = serde_json::to_string(&query).unwrap();
-
+        
         let client = reqwest::Client::new();
         let request = client
             .post(REQUEST_URL)
-            .header("Authorization", TOKEN)
+            .header("Authorization", &self.gpt_token)
             .header("Content-Type", CONTENT_TYPE)
             .body(json)
             .send();
